c1fa92bf847489ba65026c977a1863caa096f0ad
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-12-18 13:14:41 +0530

    Strings package usage
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

---
+++ 02_Basics/01_Arithmetic_Operations/06_calculator.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Calculator Application
+
+	//num1 := 100
+	//num2 := 200
+
+	var num1, num2 int32
+	fmt.Println("Enter the values of num1 and num2:")
+	fmt.Scanf("%d", &num1)
+	fmt.Scanf("%d", &num2)
+
+	fmt.Println("num1 + num2 = ", num1 + num2)
+	fmt.Println("num1 - num2 = ", num1 - num2)
+	fmt.Println("num1 * num2 = ", num1 * num2)
+	fmt.Println("num1 / num2 = ", num1 / num2)
+}
