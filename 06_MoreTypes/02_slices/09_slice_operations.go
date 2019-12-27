ab67a6106c165b8e37f8fbba58ba92edda547fc1
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-12-27 23:36:00 +0530

    slices
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

--- 06_MoreTypes/02_slices/09_slice_operations.go
+++ 06_MoreTypes/02_slices/09_slice_operations.go
@@ -1,11 +1,22 @@
 package main
 
 import "fmt"
+
 // reverse reverses a slice of ints in place.
 func reverse(s []int) {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		s[i], s[j] = s[j], s[i]
 	}
+}
+
+func leftRotation(a []int, size int, rotation int) []int {
+	var newArray []int
+	for i := 0; i < rotation; i++ {
+		newArray = a[1:size]
+		newArray = append(newArray, a[0])
+		a = newArray
+	}
+	return a
 }
 
 func main() {
@@ -23,4 +34,8 @@ func main() {
 	fmt.Println("After slice1[2:]     :", slice1) // [0 1 2 3 4 5]
 	reverse(slice1)
 	fmt.Println("After reverse(slice1):", slice1) // [2 3 4 5 0 1]
+
+	array1 = [...]int{0, 1, 2, 3, 4, 5}
+	rotation := 4
+	fmt.Println("After leftRotation", leftRotation(slice1, len(slice1), rotation))
 }
