07e276a279815f057bedfed192ec60973247586e
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2020-09-19 16:20:00 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

--- 03_Basics/01_Arithmetic_Operations/a_floating_point.go
+++ 03_Basics/01_Arithmetic_Operations/a_floating_point.go
@@ -11,6 +11,15 @@ func main() {
 	const Planck = 6.62606957e-34
 	fmt.Println(Avogadro, reflect.TypeOf(Avogadro))
 	fmt.Println(Planck, reflect.TypeOf(Planck))
+	fmt.Println()
+
+	positiveInfinity := math.Inf(0)
+	fmt.Println(positiveInfinity, reflect.TypeOf(positiveInfinity)) // +Inf float64
+	negativeInfinity := math.Inf(-98)
+	fmt.Println(negativeInfinity, reflect.TypeOf(negativeInfinity)) // -Inf float64
+	garbageValue := math.Inf
+	fmt.Println(garbageValue, reflect.TypeOf(garbageValue)) // 0xc6c5a0 func(int) float64
+	fmt.Println()
 
 	x := 1
 	fmt.Println("math.Exp(float64(x)):", math.Exp(float64(x)))
