537cddb8a0d69adfaab3e6a240185d6407ef4b9c
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2021-01-26 12:29:01 +0530

    updated

--- 09_Goroutines_Channels_Concurrency/a_goroutines/a_goroutine.go
+++ 09_Goroutines_Channels_Concurrency/a_goroutines/a_goroutine.go
@@ -1,25 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+func main() {
+	// calling a function
+	helloWorld()
 
-
-func printItem(i int) {
-	fmt.Printf("Print Item: %2v\n", i + 1)
+	// creating a go-routine
+	go helloWorld()
 }
 
-func main(){
-	fmt.Println("Start Script")
-
-	for j := 0; j < 10; j++{
-		go printItem(j)
-	}
-	fmt.Println("End Script")
-
-	// Wait, giving time for the go routines
-	// to finish.
-	time.Sleep(1000)
-
+func helloWorld(){
+	println("Hello World")
 }
