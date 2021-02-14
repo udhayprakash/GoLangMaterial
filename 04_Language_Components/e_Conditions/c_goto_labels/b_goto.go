69e7bf4ef4347d475bcb10fc732c11be97e2a9d6
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2021-02-14 11:55:37 +0530

    updated

--- 04_Language_Components/e_Conditions/c_goto_labels/b_goto.go
+++ 04_Language_Components/e_Conditions/c_goto_labels/b_goto.go
@@ -3,18 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	/* local variable definition */
-	var a int = 10
-
-	/* do loop execution */
-LOOP:
-	for a < 20 {
-		if a == 15 {
-			/* skip the iteration */
-			a = a + 1
-			goto LOOP
-		}
-		fmt.Printf("value of a: %d\n", a)
-		a++
+	i := 0
+Here:
+	fmt.Println(i)
+	i++
+	if i <= 10 {
+		goto Here
 	}
 }
