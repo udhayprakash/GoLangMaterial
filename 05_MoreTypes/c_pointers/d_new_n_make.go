fbf017e62a90276fb92db3ba260a4df03af274c7
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2021-02-15 22:13:20 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

---
+++ 05_MoreTypes/c_pointers/d_new_n_make.go
@@ -0,0 +1,39 @@
+/*
+Purpose:
+	To allocate memory Go has two primitives, new and make.
+
+	new allocates; make initializes
+
+	new(T)
+		- returns *T pointing to a zeroed T
+		- allocates zeroed storage for a new item of type T
+			and returns its address, a value of type *T.
+		- it returns a pointer to a newly allocated zero
+			value of type T. This is important to remember.
+
+	make(T, args)
+		- returns an initialized (not zero!) value of type T,
+			and not a pointer: *T.
+		- It creates slices, maps, and channels only
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(new([]int))     // &[]
+	fmt.Println(make([]int, 5)) // [0 0 0 0 0]
+
+	fmt.Println(new([]float64))     // &[]
+	fmt.Println(make([]float64, 5)) // [0 0 0 0 0]
+
+	fmt.Println(new([]string))     // &[]
+	fmt.Println(make([]string, 5)) // [     ]
+
+	fmt.Println(new([]rune))     // &[]
+	fmt.Println(make([]rune, 5)) // [0 0 0 0 0]
+
+	fmt.Println(new([]bool))     // &[]
+	fmt.Println(make([]bool, 5)) // [false false false false false]
+
+}
