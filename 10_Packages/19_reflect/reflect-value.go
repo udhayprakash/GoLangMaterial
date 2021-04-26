557a42afcd94ace3c116360e71a1923c137c178b
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2021-04-27 01:36:22 +0530

    updated

---
+++ 10_Packages/19_reflect/reflect-value.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+    "reflect"
+)
+
+func main() {
+    var a int32 = 42
+    var b string = "forty two"
+
+    valueOfA := reflect.ValueOf(a)
+	fmt.Println("a =", a, "\t\tvalueOfA=", valueOfA)
+    fmt.Println("valueOfA.Interface()=", valueOfA.Interface())
+
+    valueOfB := reflect.ValueOf(b)
+	fmt.Println("\n\nb=", b, "\tvalueOfB=", valueOfB)
+    fmt.Println("valueOfB.Interface()=", valueOfB.Interface())
+}
