eafdca3e637fd79db38d0c5bb95253f1c64c047a
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2021-04-30 23:54:13 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

--- 05_MoreTypes/d_maps/m-nil-vs-empty-map.go
+++ 05_MoreTypes/d_maps/m-nil-vs-empty-map.go
@@ -18,4 +18,14 @@ func main() {
 
 	// nilMap["one"] = true   // panic: assignment to entry in nil map
 	fmt.Println("nilMap  =", nilMap)
+
+	// === Best way to clear a map
+	m := map[string]bool{
+		"Go is awesome":             true,
+		"I drink and I know things": true,
+		"PI IS EXACTLY THREE!":      false,
+	}
+	m = map[string]bool{}
+	fmt.Println(len(m)) // 0
+
 }
