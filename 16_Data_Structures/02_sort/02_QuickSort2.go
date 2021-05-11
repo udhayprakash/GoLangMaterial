a7dbea97c7442ba058b82f962ad6f61841495f80
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2021-05-12 02:09:13 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

---
+++ 16_Data_Structures/02_sort/02_QuickSort2.go
@@ -0,0 +1,53 @@
+// Quick Sort in Golang
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+/*
+The quick sort algorithm falls under the divide and conquer class of algorithms, where we break (divide) a problem into smaller chunks that are much simpler to solve (conquer). In this case, an unsorted array is broken into sub-arrays that are partially sorted, until all elements in the list are in the right position, by which time our unsorted list will have become sorted.
+*/
+func main() {
+
+	arr := RandomArray(10)
+
+}
+
+// Generates a slice of size, size filled with random numbers
+func generateSlice(size int) []int {
+
+	slice := make([]int, size, size)
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < size; i++ {
+		slice[i] = rand.Intn(999) - rand.Intn(999)
+	}
+	return slice
+}
+
+func quicksort(a []int) []int {
+	if len(a) < 2 {
+		return a
+	}
+
+	left, right := 0, len(a)-1
+
+	pivot := rand.Int() % len(a)
+
+	a[pivot], a[right] = a[right], a[pivot]
+
+	for i, _ := range a {
+		if a[i] < a[right] {
+			a[left], a[i] = a[i], a[left]
+			left++
+		}
+	}
+
+	a[left], a[right] = a[right], a[left]
+
+	quicksort(a[:left])
+	quicksort(a[left+1:])
+
+	return a
+}
