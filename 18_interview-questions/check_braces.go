7f0e71cabaa1e4805499d6ab12da278fb815c999
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2021-08-01 17:32:37 +0530

    check  balance braces problem
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

---
+++ 18_interview-questions/check_braces.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func checkBalanceBraces(expression string) bool {
+	braces := map[rune]rune{
+		'}': '{',
+		')': '(',
+		']': '[',
+	}
+	bracesFound := []rune{}
+	for _, eachChr := range expression {
+		if strings.Contains("{([", string(eachChr)) {
+			bracesFound = append(bracesFound, eachChr)
+		} else if strings.Contains("})]", string(eachChr)) {
+			if len(bracesFound) == 0 || bracesFound[len(bracesFound)-1] != braces[eachChr] {
+				return false
+			}
+			bracesFound = bracesFound[:len(bracesFound)-1]
+		}
+	}
+	if len(bracesFound) == 0 {
+		return true
+	}
+	return false
+}
+
+func main() {
+	fmt.Println(checkBalanceBraces("(())") == true)
+	fmt.Println(checkBalanceBraces("[](){") == false)
+	fmt.Println(checkBalanceBraces("sdjf{[df]}()") == true)
+	fmt.Println(checkBalanceBraces("sdjf{[d)f]})()") == false)
+	fmt.Println(checkBalanceBraces("sdjf{[d()f]})()") == false)
+	fmt.Println(checkBalanceBraces("sdjf{[d()f]}()") == true)
+	fmt.Println(checkBalanceBraces("{{") == false)
+	fmt.Println(checkBalanceBraces("{{()") == false)
+	fmt.Println(checkBalanceBraces("{[()}]") == false)
+	fmt.Println(checkBalanceBraces("[{()()}({[]})]({}[({})])((((((()[])){}))[]{{{({({({{{{{{}}}}}})})})}}}))[][][]{") == false)
+	fmt.Println(checkBalanceBraces("[{()()}({[]})]({}[({})])((((((()[])){}))[]{{{({({({{{{{{}}}}}})})})}}}))[][][]]") == false)
+	fmt.Println(checkBalanceBraces("[{()([)}({[]})]({}[({})])((((((()[])){}))[]{{{({({({{{{{{}}}}}})})})}}}))[][][]") == false)
+	fmt.Println(checkBalanceBraces("{}[]()") == true)
+	fmt.Println(checkBalanceBraces("{[}]") == false)
+	fmt.Println(checkBalanceBraces(")") == false)
+	fmt.Println(checkBalanceBraces("}") == false)
+	fmt.Println(checkBalanceBraces("]") == false)
+	fmt.Println(checkBalanceBraces("[()]") == true)
+	fmt.Println(checkBalanceBraces("[{}]") == true)
+	fmt.Println(checkBalanceBraces("{][}") == false)
+}
