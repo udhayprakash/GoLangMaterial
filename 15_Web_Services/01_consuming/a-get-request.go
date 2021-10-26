a2d6e1cf4d129c254d4c90019459ab737752a397
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2021-10-26 22:58:51 +0530

    updated

--- 15_Web_Services/01_consuming/a-get-request.go
+++ 15_Web_Services/01_consuming/a-get-request.go
@@ -10,17 +10,13 @@ import (
 func main() {
 
 	resp, err := http.Get("http://webcode.me")
-
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
-
 	if err != nil {
-
 		log.Fatal(err)
 	}
 
