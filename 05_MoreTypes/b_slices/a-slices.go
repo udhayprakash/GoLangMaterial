07319fe713330d9fc42805f6c07786793291ab55
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2022-01-22 04:50:16 +0530

    updated

--- 05_MoreTypes/b_slices/a-slices.go
+++ 05_MoreTypes/b_slices/a-slices.go
@@ -12,14 +12,17 @@ func main() {
 	a2 := [...]int{11, 22, 33} // [11 22 33] - [3]int - array
 	fmt.Printf("%v - %[1]T - %v\n", a2, reflect.TypeOf(a2).Kind())
 
-	a3 := []int{11, 22, 33} // [11 22 33] - []int - slice
-	fmt.Printf("%v - %[1]T - %v\n", a3, reflect.TypeOf(a3).Kind())
+	s1 := []int{11, 22, 33} // [11 22 33] - []int - slice
+	fmt.Printf("%v - %[1]T - %v\n", s1, reflect.TypeOf(s1).Kind())
 
-	a4 := make([]int, 3) // [0 0 0] - []int - slice
-	fmt.Printf("%v - %[1]T - %v\n", a4, reflect.TypeOf(a4).Kind())
+	s2 := make([]int, 3) // [0 0 0] - []int - slice
+	fmt.Printf("%v - %[1]T   - %v\n", s2, reflect.TypeOf(s2).Kind())
 
 	fmt.Println("a1 == a2 :", a1 == a2) // true
 	// a1 == a3 // Can't compare array & slice
 
+	// Also, slice can be created from an array
+	var s3 []int = a1[1:3]
+	fmt.Printf("%v - %[1]T   - %v\n", s3, reflect.TypeOf(s3).Kind())
+
 }
- 
