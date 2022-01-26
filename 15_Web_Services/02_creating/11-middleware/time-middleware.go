e320f7b42a1dc3c5259855f06c50658593426448
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2022-01-27 04:01:39 +0530

    Update time-middleware.go

--- 15_Web_Services/02_creating/11-middleware/time-middleware.go
+++ 15_Web_Services/02_creating/11-middleware/time-middleware.go
@@ -1,27 +1,35 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "time"
+	"fmt"
+	"net/http"
+	"time"
 )
 
+/*
+Middleware is an entity that intercepts the server's request/response life cycle.
+  - Process the request before running business logic (authentication)
+  - Modify the request to the next handler function (attaching payload)
+  - Modify the response for the client
+  - Logging.... and much more
+
+*/
 func main() {
-  http.HandleFunc("/hello", timed(hello))
-  fmt.Println("Listening at localhost:3000")
-  http.ListenAndServe(":3000", nil)
+	http.HandleFunc("/hello", timed(hello))
+	fmt.Println("Listening at localhost:3000")
+	http.ListenAndServe(":3000", nil)
 }
 
 func timed(f func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
-  return func(w http.ResponseWriter, r *http.Request) {
-    start := time.Now()
-    f(w, r)
-    time.Sleep(2)
-    end := time.Now()
-    fmt.Println("The request took", end.Sub(start))
-  }
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		f(w, r)
+		time.Sleep(2)
+		end := time.Now()
+		fmt.Println("The request took", end.Sub(start))
+	}
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintln(w, "<h1>Hello!</h1>")
+	fmt.Fprintln(w, "<h1>Hello!</h1>")
 }
