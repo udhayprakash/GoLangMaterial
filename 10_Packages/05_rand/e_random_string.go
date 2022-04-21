e7f4527b968e7df70be9cf8f478f50a0ed07d18e
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2022-04-21 12:35:16 +0530

    updated

--- 10_Packages/05_rand/e_random_string.go
+++ 10_Packages/05_rand/e_random_string.go
@@ -7,25 +7,46 @@ import (
 )
 
 func genRandomString(strLength int) string {
-
 	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ" +
 		"abcdefghijklmnopqrstuvwxyzåäö" +
 		"0123456789")
 	buf := make([]rune, strLength)
 	for i := range buf {
 		buf[i] = chars[rand.Intn(len(chars))]
-		//fmt.Println("buf[i]:", buf[i])
 	}
-	//fmt.Println("buf        :", buf)
-	//fmt.Println("string(buf):", string(buf))
 	return string(buf)
 }
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	fmt.Println("genRandomString(10):", genRandomString(10))
-	fmt.Println("genRandomString(10):", genRandomString(10))
+	// rand.Seed(time.Now().UnixNano()) -- adding it makes unpredictable
+	fmt.Println("genRandomString(10):", genRandomString(10)) // BsD9ufNhpt
+	fmt.Println("genRandomString(10):", genRandomString(10)) // fPnZUSLy7r
 	rand.Seed(32)
-	fmt.Println("\ngenRandomString(10):", genRandomString(10))
-	fmt.Println("genRandomString(10):", genRandomString(10))
+	fmt.Println("\ngenRandomString(10):", genRandomString(10)) // jLFmRZphVf
+	fmt.Println("genRandomString(10):", genRandomString(10))   // p1kar48Air
+	fmt.Println()
+
+	// case when rand.Seed in function
+	fmt.Println("genRandomString2(10):", genRandomString2(10)) // unpredictable
+	fmt.Println("genRandomString2(10):", genRandomString2(10)) // unpredictable
+}
+
+func genRandomString2(strLength int) string {
+	rand.Seed(time.Now().UnixNano())
+	digits := "0123456789"
+	specials := "~=+%^*/()[]{}/!@#$?|"
+	all := "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+		"abcdefghijklmnopqrstuvwxyz" +
+		digits + specials
+
+	buf := make([]byte, strLength)
+	buf[0] = digits[rand.Intn(len(digits))]
+	buf[1] = specials[rand.Intn(len(specials))]
+	for i := 2; i < strLength; i++ {
+		buf[i] = all[rand.Intn(len(all))]
+	}
+	rand.Shuffle(len(buf), func(i, j int) {
+		buf[i], buf[j] = buf[j], buf[i]
+	})
+	return string(buf)
 }
