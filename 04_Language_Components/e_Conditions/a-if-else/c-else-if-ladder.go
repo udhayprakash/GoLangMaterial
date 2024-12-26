37666e979203ae5bab014c51412dc8ac53da94a5
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2024-12-26 06:38:14 +0000

    Update c-else-if-ladder.go

--- 04_Language_Components/e_Conditions/a-if-else/c-else-if-ladder.go
+++ 04_Language_Components/e_Conditions/a-if-else/c-else-if-ladder.go
@@ -5,6 +5,7 @@ import "fmt"
 // Display largest number, among two given numbers
 
 func main() {
+
 	var num1, num2 int
 
 	fmt.Println("Please enter two numbers, space separated:")
@@ -16,6 +17,16 @@ func main() {
 			num2 = %d
 	`, num1, num2)
 
+	// Method 1
+	if num1 > num2 {
+		fmt.Println("num1 is greater")
+	} else if num1 < num2 {
+		fmt.Println("num2 is greater")
+	} else { // num1 == num2
+		fmt.Println("Both are equal")
+	}
+
+	// Method 2
 	if num1 == num2 {
 		fmt.Println("Both are equal")
 	} else if num1 > num2 {
@@ -23,6 +34,7 @@ func main() {
 	} else { //  num1 < num2
 		fmt.Println("num2 is greater")
 	}
+
 }
 
 // Assignment - Display largest number, among three numbers
