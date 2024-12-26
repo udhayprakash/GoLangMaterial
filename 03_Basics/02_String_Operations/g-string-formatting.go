b709fb2a840e544a5e2e7de89224dcaedfdc69e6
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2024-12-26 04:57:01 +0000

    Update g-string-formatting.go

--- 03_Basics/02_String_Operations/g-string-formatting.go
+++ 03_Basics/02_String_Operations/g-string-formatting.go
@@ -7,12 +7,15 @@ import (
 )
 
 func main() {
+	fmt.Printf("")
+
 	// %d - decimal notation
 	fmt.Printf("%d\n", 123) // 123
 
 	// %b - binary notation
 	fmt.Printf("%b\n", 123) // 1111011
 
+	// Hexadecimal - 0 to 9, A to Z
 	fmt.Printf("%x\n", 456)        // 1c8
 	fmt.Printf("%x\n", "hex this") // 6865782074686973
 
@@ -22,27 +25,36 @@ func main() {
 	fmt.Printf("%e\n", 123.45) // 1.234500e+02
 	fmt.Printf("%E\n", 123.45) // 1.234500E+02
 
+	// rune
 	fmt.Printf("%c\n", 33) // !
 	fmt.Printf("%q\n", 33) // '!'
 
+	// string
+	fmt.Printf("%c\n", '!') // !
 	fmt.Printf("%s\n", '!') // %!s(int32=33)
 	fmt.Printf("%s\n", "!") // !
 	fmt.Println()
 
+	// formating with digit space reserved
 	fmt.Printf("|%6d |%6d|\n", 12, 12)     // |    12 |    12|
 	fmt.Printf("|%6d |%6d|\n", 12.3, 34.5) // |%!d(float64=  12.3) |%!d(float64=  34.5)|
 
-	fmt.Printf("|%6g |%6g|\n", 12.3, 34.5)       // |  12.3 |  34.5|
-	fmt.Printf("|%6f |%6f|\n", 12.3, 34.5)       // |12.300000 |34.500000|
+	fmt.Printf("|%6g |%6g|\n", 12.3, 34.5) // |  12.3 |  34.5|
+	fmt.Printf("|%6f |%6f|\n", 12.3, 34.5) // |12.300000 |34.500000|
+
+	// floating with post deciaml 2 digits only
 	fmt.Printf("|%6.2f |%6.2f|\n", 12.3, 34.5)   // | 12.30 | 34.50|
 	fmt.Printf("|%-6.2f |%-6.2f|\n", 12.3, 34.5) // |12.30  |34.50 |
 
+	fmt.Printf("|%-06.2f |%-06.2f|\n", 12.3, 34.5) // |12.30  |34.50 |
+	fmt.Printf("|%06.2f |%06.2f|\n", 12.3, 34.5)   // |012.30 |034.50 |
 	fmt.Println()
 
 	fmt.Printf("|%6s|%6s|\n", "foo", "b")   // |   foo|     b|
 	fmt.Printf("|%-6s|%-6s|\n", "foo", "b") // |foo   |b     |
 	fmt.Println()
 
+	// creating new string from formatting
 	myStr := fmt.Sprintf("A %s baby was born %s\n", "beutiful", "today")
 	fmt.Println(myStr) // A beutiful baby was born today
 
@@ -64,6 +76,5 @@ func main() {
 
 	fmt.Println("reflect.TypeOf(p):", reflect.TypeOf(p1)) //  main.point
 	fmt.Printf("%T\n", p1)                                // main.point
-	fmt.Printf("%t\n", true)                              // true
-
+	fmt.Printf("%t\n", true)
 }
