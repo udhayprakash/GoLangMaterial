584061775e600f25cb97cdf548a992ae0a1bc6d6
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2024-12-26 08:35:22 +0000

    Update h-palindrome-numbers.go

--- 04_Language_Components/f_Loops/a_for_loop/h-palindrome-numbers.go
+++ 04_Language_Components/f_Loops/a_for_loop/h-palindrome-numbers.go
@@ -5,30 +5,52 @@ import (
 )
 
 // Get palindrome number between 100 & 999
-// Ex: 101, 111, ... 888, ... 999
+// Ex: 101, 111, ... 888, ...
 
 func main() {
-	// for num:= 0; num <= 999; num++{
-	// }
 
-	num := 6776
-	orgNum := num
-	revNum := 0
-	for num != 0 {
-		fmt.Println("\nnum /10=", num/10)
-		fmt.Println("num %10=", num%10)
+	for num := 100; num < 1000; num++ {
 
-		revNum += (num % 10)
+		// num := 6776
+		// orgNum := num
+		// revNum := 0
 
-		num /= 10
+		// num := 6776
+
+		num := num  // safe copy of the loop scope variable
+
+		orgNum, revNum := num, 0
+
+		for num != 0 {
+			lastDigit := num % 10
+			// fmt.Println("lastDigit is ", lastDigit)
+			// fmt.Println("remaining", num/10)
+
+			// revNum += lastDigit
+			revNum = ((revNum * 10) + lastDigit)
+
+			num /= 10 // num = num /10 compound operator
+		}
+
+		// fmt.Println("\norgNum =", orgNum)
+		// fmt.Println("revNum =", revNum)
+
+		if orgNum == revNum {
+			fmt.Printf("%d is palindrome\n", orgNum)
+		}
 	}
-
-	fmt.Println("\norgNum =", orgNum)
-	fmt.Println("revNum =", revNum)
 }
 
-//  6776
-// 6000
-//  700
-//   70
-//    6
+/*
+6776	6
+677		67
+		(6 * 10) + 7
+67		677
+		((6 * 10) + 7) * 10 + 7
+6		6776
+		(((6 * 10) + 7) * 10 + 7) * 10 + 6
+
+*/
+
+// NOTE: variable defined inside a loop has loop scope
+// NOTE:  you're modifying the num variable inside the loop, but then using it again in the outer loop
