b4e0ad36ea67acdef0f7a8cb45ed99332a291f76
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2024-12-26 07:35:21 +0000

    Update b-get-even-numbers.go

--- 04_Language_Components/f_Loops/a_for_loop/b-get-even-numbers.go
+++ 04_Language_Components/f_Loops/a_for_loop/b-get-even-numbers.go
@@ -8,10 +8,12 @@ func main() {
 	fmt.Println("Even number between 56 & 100:")
 
 	for num := 56; num <= 100; num++ {
-		if (num%2 == 0) == true {
+		if num%2 == 0 {
+			// fmt.Println(num)
 			fmt.Print(num, " ")
 		}
 	}
+
 	fmt.Println()
 
 	for num := 56; num <= 100; num++ {
@@ -19,7 +21,13 @@ func main() {
 			fmt.Print(num, " ")
 		}
 	}
+
+	// fmt.Println("num = ", num)
+	// undefined: num
 }
 
 //  golang supports only ->  i++,  i--
 //  doesnt support       ->  ++i, --i
+
+
+// variable defined in for loop definition , cant be accessed outside 
