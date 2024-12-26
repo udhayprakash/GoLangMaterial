c586088490f549898545f32b3c41d30c1418a3a7
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2024-12-26 08:59:56 +0000

    Update f-for-range-is-copy.go

--- 04_Language_Components/g_range/f-for-range-is-copy.go
+++ 04_Language_Components/g_range/f-for-range-is-copy.go
@@ -10,4 +10,14 @@ func main() {
 		v *= 2
 	}
 	fmt.Println(evenVals) // [2 4 6 8 10 12]
+
+	for index, v := range evenVals {
+
+		// modifies the orginal loop list
+		evenVals[index] = v * 2
+
+	}
+	fmt.Println(evenVals) // [4 8 12 16 20 24]
 }
+
+// NOTE: Never change the values of a iterables, on which you are looping
