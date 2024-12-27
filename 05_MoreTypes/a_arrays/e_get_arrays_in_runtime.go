a85c9c2371d4cca45590c31ebe53fd2c14662432
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2024-12-27 05:23:06 +0000

    Update e_get_arrays_in_runtime.go

--- 05_MoreTypes/a_arrays/e_get_arrays_in_runtime.go
+++ 05_MoreTypes/a_arrays/e_get_arrays_in_runtime.go
@@ -5,20 +5,22 @@ import "fmt"
 func main() {
 	var numbers [10]int // array
 
-	// WRONG APPROACH
+	// // WRONG APPROACH
 	// fmt.Println("Enter 10 numbers:")
 	// _, err := fmt.Scanf("%v", &numbers)
 	// if err != nil {
 	// 	fmt.Println(err) // can't scan type: *[10]int
+	// } else {
+	// 	fmt.Println("numbers=", numbers)
 	// }
-	// fmt.Println("numbers=", numbers)
 
-	// Correct way
+	// correct way
 	for index := 0; index < len(numbers); index++ {
 		fmt.Printf("Enter %d position value:", index)
 		fmt.Scanln(&numbers[index])
 	}
 	fmt.Println("numbers=", numbers)
+
 }
 
-// Asignment - try the same using range
+// Assignment - try the same using range
