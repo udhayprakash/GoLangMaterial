91ed1611fca339b6719406bbb6cb434f31c1ff5b
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2024-12-27 05:55:32 +0000

    Update d-2d-array.go

--- 05_MoreTypes/b_slices/d-2d-array.go
+++ 05_MoreTypes/b_slices/d-2d-array.go
@@ -6,7 +6,6 @@ import (
 )
 
 func main() {
-
 	var rows, columns int
 
 	fmt.Println("Creating a 2 dimensional array of integer type ")
@@ -16,27 +15,25 @@ func main() {
 	fmt.Print("Enter number of columns : ")
 	fmt.Scan(&columns)
 
-	// allocate memory for 2 dimension array of integer type with
+	// allocate memory for 2 dimension slice of integer type with
 	// [][]int
 	// and use number of rows to determine the size
-	array := make([][]int, rows)
+	myslice := make([][]int, rows)
 
-	// followed by columns
-	for i := range array {
-		array[i] = make([]int, columns)
+	for i := range myslice {
+		myslice[i] = make([]int, columns)
 	}
+	fmt.Println("myslice = ", myslice)
 
-	array[0][0] = 0 // in progamming world, starting counting from zero
+	myslice[0][0] = rows * columns
+	myslice[rows-1][columns-1] = rows * columns
+	fmt.Println("myslice = ", myslice)
 
-	array[rows-1][columns-1] = rows * columns // assign value
-
-	// print all assigned values by rows
-
-	for r := range array {
-		fmt.Printf("row(%d)  -  %d\n", r, array[r])
+	for r := range myslice {
+		fmt.Printf("row(%d)  -  %d\n", r, myslice[r])
 	}
 
 	// trigger manual garbage collection to reclaim allocated memory
-	array = nil
+	myslice = nil
 	debug.FreeOSMemory()
 }
