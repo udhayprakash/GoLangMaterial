c192138498820ebaf98f36d81a24ab4ca667242a
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2024-12-27 05:03:05 +0000

    Update b_arrays.go

--- 05_MoreTypes/a_arrays/b_arrays.go
+++ 05_MoreTypes/a_arrays/b_arrays.go
@@ -11,8 +11,10 @@ func main() {
 	a2 := [3]int{2, 3, 1}
 	fmt.Println("a1 == a2 :", a1 == a2) // false
 
-	// a3 := [4]int{1, 2, 3}
-	// fmt.Println("a1 == a3 :", a1 == a3) // mismatched types [3]int and [4]int
+	a3 := [4]int{1, 2, 3}
+	fmt.Println("a3 = ", a3)
+	// fmt.Println("a1 == a3 :", a1 == a3)
+	// invalid operation: a1 == a3 (mismatched types [3]int and [4]int)
 
 	a4 := [...]int{1, 2, 3}
 	fmt.Println("a1 == a4 :", a1 == a4) // true
@@ -21,17 +23,18 @@ func main() {
 	q := [3]int{1, 2, 3}
 	fmt.Println("q =", q) // [1 2 3]
 
+	// updating the array values, in full - Overwriting the values
 	q = [3]int{44, 55, 66}
 	fmt.Println("q =", q) // [44 55 66]
 
 	// q = [2]int{777, 888}
-	// cannot use [2]int{…} (value of type [2]int) as type [3]int in assignment
+	// cannot use [2]int{…} (value of type [2]int) as [3]int value in assignment
 
 	q = [3]int{777, 888}
 	fmt.Println("q =", q) // [777 888 0]
 
-	//q = [5]int{11, 22, 33, 44, 55}
-	// cannot use [5]int literal (type [5]int) as type [3]int in assignment
+	// q = [5]int{11, 22, 33, 44, 55}
+	// cannot use [5]int{…} (value of type [5]int) as [3]int value in assignment
 
 	//q = [3]float64{4.4, 5.5, 6.6}
 	// cannot use [3]float64 literal (type [3]float64) as type [3]int in assignment
@@ -47,12 +50,12 @@ func main() {
 	fmt.Println("q[1] =", q[1]) // 89
 	fmt.Println("q =", q)       // 	[777 89 0]
 
-	// Slicing -- withh get till the last position, excluding last position
+	// Slicing -- will get till the last position, excluding last position
 	fmt.Println("q[0:2] =", q[0:2]) // [777 89]
 
 	// updating a slice of array
-	// q[0:2] = [2]int{11, 22}  //- CANT UPDATE SLICE in values in ARRAY
-	// cannot assign to q[0:2]
+	// q[0:2] = [2]int{11, 22}
+	// cannot assign to q[0:2] (neither addressable nor a map index expression)
 }
 
 /*
