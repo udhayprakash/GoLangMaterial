9b6c76dc68c93026e619cfbb24acbf4c0d1d30db
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2024-12-29 03:04:17 +0000

    Update 2-custom-error.go

--- 06_Error_Handling/c-errors/2-custom-error.go
+++ 06_Error_Handling/c-errors/2-custom-error.go
@@ -6,22 +6,19 @@ import (
 
 type MyError struct {
 	message string
+	code    int
 }
 
 func (e MyError) Error() string {
-	return fmt.Sprintf("ERROR:%v", e.message)
+	return fmt.Sprintf("ERROR:%v (Code: %d)", e.message, e.code)
 }
 
 func someError() error {
-	return MyError{"Some Error Occurred!!!"}
+	return MyError{"Some Error Occurred!!!", 1001}
 }
 
 func main() {
-	someError()
-
 	if err := someError(); err != nil {
 		fmt.Println("An error occurred:", err)
-		return
 	}
-	fmt.Println("All's Good")
 }
