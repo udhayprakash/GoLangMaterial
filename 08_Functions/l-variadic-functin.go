99af7b9a67f2f158d2abdc48cea493a4b993b718
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2024-12-30 06:09:42 +0000

    Update l-variadic-functin.go

--- 08_Functions/l-variadic-functin.go
+++ 08_Functions/l-variadic-functin.go
@@ -7,6 +7,30 @@ func length(args ...int) int {
 }
 
 func main() {
+	// slice
 	vs := []int{24, 55, 88, 99, 4, 2}
 	fmt.Println(length(vs...))
+
+
+	vs = []int{24, 55,}
+	fmt.Println(length(vs...))
+
+	
+	vs = []int{24}
+	fmt.Println(length(vs...))
+
+	vs = []int{}
+	fmt.Println(length(vs...))
+	fmt.Println()
+
+	// direct values
+	fmt.Println(length(12))
+	fmt.Println(length(24, 55, 88, 99, 4, 2))
+
+	// fmt.Println(length(213.213, 213.213. 213))
+
+	// myarray := [4]int{12, 33, 45, 676}
+	// fmt.Println(length(myarray...))
+	// cannot use myarray (variable of type [4]int) as []int value in argument to length
+
 }
