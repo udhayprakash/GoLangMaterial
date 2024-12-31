6e7d08055373161bbcb099cac88ed2a003fb68cd
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2024-12-31 06:12:05 +0000

    Update db_waitgroups_in_inline_functions.go

--- 09_Goroutines_Channels_Concurrency/c-waitgroups/a_basics/db_waitgroups_in_inline_functions.go
+++ 09_Goroutines_Channels_Concurrency/c-waitgroups/a_basics/db_waitgroups_in_inline_functions.go
@@ -8,15 +8,16 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
-	wg.Add(1)
 
+	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		fmt.Println("1st goroutine sleeping...")
 		time.Sleep(1)
 	}()
-	wg.Add(1)
 
+
+	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		fmt.Println("2nd goroutine sleeping...")
