1aed170bddd8fb3b0543656563c589afda9e2196
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2025-01-01 05:52:17 +0000

    Update c-lookpath.go

--- 10_Packages/01_OS/d-manage-processes/c-lookpath.go
+++ 10_Packages/01_OS/d-manage-processes/c-lookpath.go
@@ -8,27 +8,29 @@ import (
 )
 
 func main() {
+	// which go (or) where go
 	pathInfo, err := exec.LookPath("go")
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("pathInfo:", pathInfo)
 
-	binary, lookErr := exec.LookPath("ls")
+	ls_binary_path, lookErr := exec.LookPath("ls")
 	if lookErr != nil {
 		panic(lookErr)
 	}
-	fmt.Println("binary=", binary)
+	fmt.Println("ls_path=", ls_binary_path)
 
-	args := []string{"ls", "-a", "-l", "-h"}
+	args := []string{"-a", "-l", "-h"}
 
 	env := os.Environ()
 
-	execErr := syscall.Exec(binary, args, env)
+	execErr := syscall.Exec(ls_binary_path, args, env)
 	if execErr != nil {
 		panic(execErr)
 	}
 }
 
-//C:\Program Files (x86)\Gow\bin\ls.exe ls -a -l -h
-//ls -a -l -h
+// ls -a -l -h
+// /usr/bin/ls -a -l -h
+// C:\Program Files (x86)\Gow\bin\ls.exe ls -a -l -h
