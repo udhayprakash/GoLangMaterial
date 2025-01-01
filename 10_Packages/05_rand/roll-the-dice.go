ab7fc5595e90ab8555720a9ae117785a8e07b9a0
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2025-01-01 07:47:29 +0000

    Update roll-the-dice.go

--- 10_Packages/05_rand/roll-the-dice.go
+++ 10_Packages/05_rand/roll-the-dice.go
@@ -7,20 +7,26 @@ import (
 	"time"
 )
 
-var onlyOnce sync.Once
-
 // prepare the dice
 var dice = []int{1, 2, 3, 4, 5, 6}
 
+var once sync.Once
+
 func rollDice() int {
+
 	// rand.Seed(time.Now().UnixNano())
 	// fmt.Println(1 + rand.Intn(6-1))
 
-	onlyOnce.Do(func() {
+	once.Do(func() {
 		rand.Seed(time.Now().UnixNano()) // only run once
 	})
+	
+	// fmt.Println(rand.IntN(6))
+	// fmt.Println(rand.IntN(len(dice)))
+	number := rand.Intn(len(dice))
+	// fmt.Println(dice[number])
 
-	return dice[rand.Intn(len(dice))]
+	return dice[number]
 }
 
 func main() {
