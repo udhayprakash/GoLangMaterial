b652a48bcc0398a1b3c106ccb882711164ba2b89
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2025-01-01 04:57:24 +0000

    Update c-flag-boolean.go

--- 10_Packages/01_OS/c-flag-related/c-flag-boolean.go
+++ 10_Packages/01_OS/c-flag-related/c-flag-boolean.go
@@ -8,12 +8,23 @@ import (
 
 func main() {
 	var toUpper bool
-	flag.BoolVar(&toUpper, "u", false, "Should the words be converted to upper case")
+	flag.BoolVar(&toUpper, "u", false, "should the wordls be converted to upper case")
 
 	flag.Parse()
 
 	words := flag.Args()
 
+	// // Method 1 - expensive operation
+	// for _, word := range words {
+	// 	if toUpper == true {
+	// 		fmt.Println(strings.ToUpper(word))
+	// 	} else {
+	// 		fmt.Println(word)
+	// 	}
+
+	// }
+
+	// Method 2 - optimal
 	if toUpper == true {
 		for _, word := range words {
 			fmt.Println(strings.ToUpper(word))
@@ -26,23 +37,19 @@ func main() {
 
 }
 
+
 /*
 OUTPUT:
--------
-    ~go run e-flag-boolean.go
-    ~go run e-flag-boolean.go -h
-    Usage of C:\Users\Amma\AppData\Local\Temp\go-build4183878898\b001\exe\e-flag-boolean.exe:  -u    Should the words be converted to upper case
-    ~go run e-flag-boolean.go -u
-
-	~go run e-flag-boolean.go -u=true
-
-    ~go run e-flag-boolean.go -u true
-    TRUE
-
-    ~go run e-flag-boolean.go -u=true apple mango goa banana
-    APPLE
-    MANGO
-    GOA
-    BANANA
-
+------
+$ go run c-flag-boolean.go 
+$ go run c-flag-boolean.go -h
+Usage of /tmp/go-build3541989184/b001/exe/c-flag-boolean:
+  -u    should the wordls be converted to upper case
+$ go run c-flag-boolean.go -u
+$ go run c-flag-boolean.go -u=true
+$ go run c-flag-boolean.go -u=true apple mango goa banana
+APPLE
+MANGO
+GOA
+BANANA
 */
