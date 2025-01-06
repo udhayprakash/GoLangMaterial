3ed3172077df131083018cccd717a26bec2f6471
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2025-01-06 06:44:27 +0000

    Update e_LogRedirects.go

--- 12_Logging/a_builtin_logger/e_LogRedirects.go
+++ 12_Logging/a_builtin_logger/e_LogRedirects.go
@@ -8,10 +8,16 @@ import (
 func main() {
 	log.Println("message 1")
 
-	file, e := os.OpenFile("e_LogRedirects.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	file, e := os.OpenFile(
+		"e_LogRedirects.log",
+		os.O_CREATE|os.O_WRONLY|os.O_APPEND,
+		0666)
+
 	if e != nil {
 		log.Fatalln("Failed to open log file")
 	}
+	log.Println(file)
+
 	// Redirecting logs to the file handler
 	log.SetOutput(file)
 
