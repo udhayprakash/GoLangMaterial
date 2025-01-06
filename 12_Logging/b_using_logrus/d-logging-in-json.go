b17fefe7eb7353c20068bf38062ad6baff97f87e
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2025-01-06 07:13:09 +0000

    Update d-logging-in-json.go

--- 12_Logging/b_using_logrus/d-logging-in-json.go
+++ 12_Logging/b_using_logrus/d-logging-in-json.go
@@ -5,11 +5,18 @@ import (
 )
 
 func main() {
+
+	log.Println("Hello world1")
+
 	log.SetFormatter(&log.JSONFormatter{})
+	log.Println("Hello world2")
+
+	// adding fields to log
 	log.WithFields(
 		log.Fields{
 			"foo": "foo",
 			"bar": "bar",
 		},
 	).Info("Something happened")
+
 }
